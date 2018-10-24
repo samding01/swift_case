4d1e8e081e221791c97aab0bde70dfe8c25525de
root <root@csz25138.canlab.ibm.com>
2018-10-24 13:54:32 -0400

    add files

---
+++ sample1.swift
@@ -0,0 +1,12 @@
+
+
+public enum LinkParameter: String {
+    case val 
+    case type
+    case rel 
+}
+ var linkParameters   = [LinkParameter.rel:"root"]   
+ var headerValue = ""
+ for (linkParamer, value  ) in linkParameters {
+   let tmp = linkParamer
+ }
